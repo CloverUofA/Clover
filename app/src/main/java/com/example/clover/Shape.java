0352dc20622dbe2b4d7411f5b82c201fda6387cd
Mmelve Talite <ememtalite@yahoo.ca>
2024-09-25 21:02:19 -0600

    edited Shape class

--- app/src/main/java/com/example/clover/Shape.java
+++ app/src/main/java/com/example/clover/Shape.java
@@ -4,6 +4,11 @@ public abstract class Shape {
     protected int x;
     protected int y;
 
+    public Shape() {
+        this.x = 0;
+        this.y = 0;
+    }
+
     public Shape(int x, int y){
         this.x = x;
         this.y = y;
