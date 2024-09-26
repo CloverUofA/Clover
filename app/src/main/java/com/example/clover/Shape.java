6a8a06200de12171dbf9957a482381147834063a
Mmelve Talite <ememtalite@yahoo.ca>
2024-09-25 20:51:34 -0600

    edited Shape class

--- app/src/main/java/com/example/clover/Shape.java
+++ app/src/main/java/com/example/clover/Shape.java
@@ -1,11 +1,27 @@
 package com.example.clover;
 
-public class Shape {
-    private int x;
-    private int y;
+public abstract class Shape {
+    protected int x;
+    protected int y;
 
     public Shape(int x, int y){
         this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
         this.y = y;
     }
 }
