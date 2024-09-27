c129043ba68907065a61948198155f6e0c6508d7
seyi <oluwaseyifunmibamijoko@gmail.com>
2024-09-26 22:39:25 -0600

    Modified shape class to include color locally

--- app/src/main/java/com/example/clover/Shape.java
+++ app/src/main/java/com/example/clover/Shape.java
@@ -3,7 +3,7 @@ package com.example.clover;
 public abstract class Shape {
     protected int x;
     protected int y;
-    String color = "pruple";
+    String color = "yellow";
     public Shape() {
         this.x = 0;
         this.y = 0;
